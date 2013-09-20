95d1c822eab0ce7200c5b6862f8f859e380aa234
RouxRC <b.ooghe@gmail.com>
2013-09-20 16:00:46 +0200

    check existence

--- update_local_conf.sh
+++ update_local_conf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 for file in gitconfig vimrc bash_aliases bashrc; do
-  cp -f $HOME/.$file{,.backup-`date +%Y%m%d-%H%M`}
+  if test -f $HOME/.$file; then
+    cp -f $HOME/.$file{,.backup-`date +%Y%m%d-%H%M`}
+  fi
   cp -f {,$HOME/}.$file
 done
